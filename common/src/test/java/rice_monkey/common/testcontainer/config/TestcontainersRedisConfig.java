e4a003a84234ede34c492827fb18f967b94fd4d8
HeeSung Choi <gladhee@icloud.com>
2025-07-04 15:02:32 +0900

    feat(testcontainer): MySQL, Redis TestContainer 설정 추가 밑 공급 어노테이션 구현

---
+++ common/src/test/java/rice_monkey/common/testcontainer/config/TestcontainersRedisConfig.java
@@ -0,0 +1,30 @@
+package rice_monkey.common.testcontainer.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+
+@TestConfiguration
+public class TestcontainersRedisConfig {
+
+    @Container
+    private static final GenericContainer<?> REDIS_CONTAINER = new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+            .withExposedPorts(6379);
+
+    static {
+        REDIS_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host", REDIS_CONTAINER::getHost);
+        registry.add("spring.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379).toString());
+        registry.add("spring.redis.password", () -> "");
+        registry.add("spring.redis.ssl", () -> "false");
+    }
+
+}
