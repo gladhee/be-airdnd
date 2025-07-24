4e6d89495c3b0f17c8f4260b5d33356b9563019e
HeeSung Choi <gladhee@icloud.com>
2025-07-24 15:28:32 +0900

    feat(tests): 컨택스트 로드 테스트에서 MySQL 테스트컨테이너 통합

--- image/src/test/java/rice_monkey/image/ImageApplicationTests.java
+++ image/src/test/java/rice_monkey/image/ImageApplicationTests.java
@@ -2,9 +2,12 @@ package rice_monkey.image;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import testcontainer.config.MySQLTestContainer;
 
+@Testcontainers
 @SpringBootTest
-class ImageApplicationTests {
+class ImageApplicationTests implements MySQLTestContainer {
 
 	@Test
 	void contextLoads() {
--- member/src/test/java/rice_monkey/member/MemberApplicationTests.java
+++ member/src/test/java/rice_monkey/member/MemberApplicationTests.java
@@ -2,9 +2,12 @@ package rice_monkey.member;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import testcontainer.config.MySQLTestContainer;
 
+@Testcontainers
 @SpringBootTest
-class MemberApplicationTests {
+class MemberApplicationTests implements MySQLTestContainer {
 
 	@Test
 	void contextLoads() {
