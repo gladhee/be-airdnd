ef6be07cb8bab5f0a79b0428691defe3da251830
HeeSung Choi <gladhee@icloud.com>
2025-07-24 15:28:32 +0900

    feat(tests): 컨택스트 로드 테스트에서 MySQL 테스트컨테이너 통합

--- booking/src/test/java/rice_monkey/booking/BookingApplicationTests.java
+++ booking/src/test/java/rice_monkey/booking/BookingApplicationTests.java
@@ -2,9 +2,13 @@ package rice_monkey.booking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import testcontainer.config.MySQLTestContainer;
+import testcontainer.config.RedisTestContainer;
 
+@Testcontainers
 @SpringBootTest
-class BookingApplicationTests {
+class BookingApplicationTests implements MySQLTestContainer, RedisTestContainer {
 
 	@Test
 	void contextLoads() {
--- payment/src/test/java/rice_monkey/payment/PaymentApplicationTests.java
+++ payment/src/test/java/rice_monkey/payment/PaymentApplicationTests.java
@@ -2,9 +2,12 @@ package rice_monkey.payment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import testcontainer.config.MySQLTestContainer;
 
+@Testcontainers
 @SpringBootTest
-class PaymentApplicationTests {
+class PaymentApplicationTests implements MySQLTestContainer {
 
 	@Test
 	void contextLoads() {
