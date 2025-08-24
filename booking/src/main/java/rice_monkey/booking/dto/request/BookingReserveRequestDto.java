a69393265c4e7661c65140f7107393214bfc8bd7
HeeSung Choi <gladhee@icloud.com>
2025-08-24 21:31:57 +0900

    feat(BookingReserveRequestDto): Validation annotation 추가해서 예외 확립

--- booking/src/main/java/rice_monkey/booking/dto/request/BookingReserveRequestDto.java
+++ booking/src/main/java/rice_monkey/booking/dto/request/BookingReserveRequestDto.java
@@ -1,11 +1,14 @@
 package rice_monkey.booking.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDate;
 
 public record BookingReserveRequestDto(
-        long listingId,
-        LocalDate checkin,
-        LocalDate checkout,
-        int guestCount
+        @NotNull @Positive Long listingId,
+        @NotNull LocalDate checkin,
+        @NotNull LocalDate checkout,
+        @NotNull @Positive Integer guestCount
 ) {
 }
